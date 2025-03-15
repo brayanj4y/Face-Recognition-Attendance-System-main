6493fba2c622625b14d2f24c944bb5f528850636
Souop Sylvain Brayan <157296274+brayanj4y@users.noreply.github.com>
2025-03-15 11:38:03 +0100

    Update database_connection.php

--- database/database_connection.php
+++ database/database_connection.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 
 //your database name
-$database = "attendance-dbs";
+$database = "attendance-db";
 
 //database user which by default is root unless you have configured with another name
 $user = "root";
